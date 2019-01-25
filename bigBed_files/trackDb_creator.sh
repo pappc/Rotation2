f9033621c6c56d3d0c87cac965848ecfad335723
pappc <pappc@upstate.edu>
2019-01-25 10:06:25 -0500

    updates

--- bigBed_files/trackDb_creator.sh
+++ bigBed_files/trackDb_creator.sh
@@ -7,5 +7,5 @@ for i in $wd; do
 	url=`ls $i | cut -d"/" -f7`
 	shortLabel=`ls $i | cut -d"." -f1 | cut -d"_" -f4,5`
 	longLabel=`ls $i | cut -d"." -f1,2,3 | cut -d"/" -f7`
-	echo -e "track" $track '\n'"bigDataUrl" $url '\n'"shortLabel" $shortLabel '\n'"longLabel" $longLabel '\n'"type" "bigbed 5" '\n'"useScore" "1" '\n' >>$output
+	echo -e "track" $track '\n'"bigDataUrl" $url '\n'"shortLabel" $shortLabel '\n'"longLabel" $longLabel '\n'"type" "bigBed 5" '\n'"useScore" "1" '\n' >>$output
 done
